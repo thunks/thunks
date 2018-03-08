ed5266109f45257436ef23b75cc476607e7b9187
zensh <admin@zensh.com>
2018-03-08 22:22:10 +0800

    fixed test.

--- test/index.js
+++ test/index.js
@@ -2,10 +2,14 @@
 /* global */
 
 var thunks = require('..')
-var thunksM = require('@std/esm')(module)('../index.mjs').default
-
 require('./thunk.js')(thunks)
-require('./thunk.js')(thunksM)
+
+var thunksM
+// @std/esm can't support v4
+if (!process.version.startsWith('v4.')) {
+  thunksM = require('@std/esm')(module)('../index.mjs').default
+  require('./thunk.js')(thunksM)
+}
 
 var supportGeneratorFn = false
 var supportAsyncFn = false
@@ -20,7 +24,7 @@ try {
 
 if (supportGeneratorFn) {
   require('./generator.js')(thunks)
-  require('./generator.js')(thunksM)
+  if (thunksM) require('./generator.js')(thunksM)
 } else {
   var fileName = './test/generator.js'
   var fs = require('fs')
@@ -30,10 +34,10 @@ if (supportGeneratorFn) {
   content = regenerator.compile(content, {includeRuntime: true}).code
   var m = module._compile(content, fileName)
   m(thunks)
-  m(thunksM)
+  if (thunksM) m(thunksM)
 }
 
 if (supportAsyncFn) {
   require('./async.js')(thunks)
-  require('./async.js')(thunksM)
+  if (thunksM) require('./async.js')(thunksM)
 }
